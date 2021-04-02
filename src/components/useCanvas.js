52bba6a857caf927a7593a13392c32bd3cc7ec44
Anthony Ingle <ingleanthony@icloud.com>
2021-04-02 12:07:26 -0400

    something

--- src/components/useCanvas.js
+++ src/components/useCanvas.js
@@ -19,7 +19,7 @@ const useCanvas = (draw, options={}) => {
     return () => {
       window.cancelAnimationFrame(animationFrameId)
     }
-  }, [draw])
+  }, [draw, options.context])
   return canvasRef
 }
 export default useCanvas
