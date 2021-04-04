e1646dd7b8f6a66cba77fa16e29da383b8ae1f46
ingleanthony <ingle.anthony@icloud.com>
2021-04-04 11:56:21 -0400

    AI tensorflow demo works!

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,24 +1,33 @@
 import React, { useEffect, useRef, useState } from 'react';
 import '../css/demo.css';
+import * as tf from '@tensorflow/tfjs';
 
 export default function Demo() {
 
     const canvasRef = useRef(null);
     const contextRef = useRef(null);
+    const imageRef = useRef(null);
     const [isDrawing, setIsDrawing] = useState(false);
+    const [prediction, setPrediction] = useState(null);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
-        canvas.width = 500;
-        canvas.style.width = '250px';
-        canvas.height = 500;
-        canvas.style.height = '250px';
+        canvas.width = 560;
+        canvas.style.width = '280px';
+        canvas.height = 560;
+        canvas.style.height = '280px';
         context.lineCap = 'round';
         context.strokeStyle = 'gray';
         context.scale(2, 2);
-        context.lineWidth = 10;
+        context.lineWidth = 15;
         contextRef.current = context;
+
+        const canvas2 = imageRef.current;
+        canvas2.width = 28;
+        canvas2.style.width = '28px';
+        canvas2.height = 28;
+        canvas2.style.height = '28px';
     }, [])
 
     const startDrawing = ({ nativeEvent }) => {
@@ -38,23 +47,73 @@ export default function Demo() {
       const finishDrawing = () => {
         setIsDrawing(false);
         contextRef.current.closePath();
+        getImageData();
       };
 
       const clearCanvas = () => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        contextRef.current.clearRect(0, 0, context.canvas.height * 10, context.canvas.width * 10);
       };
+
+      const getImageData = () => {
+        const canvas = imageRef.current;
+        const context = canvas.getContext('2d');
+        canvas.width = 28;
+        canvas.style.width = '28px';
+        canvas.height = 28;
+        canvas.style.height = '28px';
+        context.drawImage(canvasRef.current, 0, 0, 28, 28);
+        let imgData = context.getImageData(0, 0, context.canvas.height, context.canvas.width);
+        predict(imgData);
+      };
+
+      async function predict(imageData) {
+        const model = await tf.loadLayersModel('model.json');
+        await tf.tidy(() => {
+
+          // Convert the canvas pixels to a Tensor of the matching shape
+          let img = tf.browser.fromPixels(imageData, 1);
+          img = img.reshape([1, 28, 28, 1]);
+          img = tf.cast(img, 'float32');
+    
+          // Make and format the predications
+          const output = model.predict(img);
+    
+          // Save predictions on the component
+        const predictions = Array.from(output.dataSync())
+          console.log(predictions);
+          let max = 0;
+          for (let i = 0; i < predictions.length; i++) {
+              if (predictions[i] > max) max = i;
+          }
+          setPrediction(max);
+        });
+    
+      }    
 
     return (
         <div className='canvasDiv'>
-            <canvas id='canvas'
-            onMouseDown = {startDrawing}
-            onMouseUp = {finishDrawing}
-            onMouseMove = {draw}
-            ref = {canvasRef}
-            />
-            <button onClick={clearCanvas}>Clear</button>
+            <h1 className='title'>Digit Recognizer AI</h1>
+            <div className='canvasFlex'>
+                <div className='canvasColumn'>
+                    <h2>Draw a digit 0-9 here:</h2>
+                    <canvas id='canvas'
+                        onMouseDown={startDrawing}
+                        onMouseUp={finishDrawing}
+                        onMouseMove={draw}
+                        ref={canvasRef}
+                    />
+
+                    <br />
+                    <button className='clearButton' onClick={clearCanvas}>Clear</button>
+                </div>
+                <div className='predictionColumn'>
+                    <canvas id='smallCanvas'
+                        ref={imageRef} />
+                    <h1>Prediction: {prediction}</h1>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,14 @@ import '../css/home.css';
 import Card from '../components/Card';
 import robot from '../images/robot.jpg';
 import infographic from '../images/ai-infographic.jpg';
-import construction from '../images/const.png';
+import digits from '../images/digits.png';
 
 export default function Home() {
     return (
         <div className="card-container">
             <Card title='Ethics of Artificial Intelligence' src={robot} to='/post'/>
             <Card title='Artificial Intelligence Infographic' src={infographic} to='/infographic'/>
-            <Card title='AI Demo!' src={construction} to='/demo'/>
+            <Card title='AI Demo!' src={digits} to='/demo'/>
         </div>
     )
 }
