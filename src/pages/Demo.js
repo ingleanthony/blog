39a113067c834ab2650c82c17c79678858400ca5
ingleanthony <ingle.anthony@icloud.com>
2021-04-03 23:43:30 -0400

    added drawable canvas

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,21 +1,60 @@
-import React from 'react'
-import Canvas from '../components/Canvas'
-import '../css/demo.css'
+import React, { useEffect, useRef, useState } from 'react';
+import '../css/demo.css';
 
 export default function Demo() {
 
-    const draw = (ctx, frameCount) => {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-        ctx.fillStyle = '#000000'
-        ctx.beginPath()
-        ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-        ctx.fill()
-      }
+    const canvasRef = useRef(null);
+    const contextRef = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        canvas.width = 500;
+        canvas.style.width = '250px';
+        canvas.height = 500;
+        canvas.style.height = '250px';
+        context.lineCap = 'round';
+        context.strokeStyle = 'gray';
+        context.scale(2, 2);
+        context.lineWidth = 10;
+        contextRef.current = context;
+    }, [])
+
+    const startDrawing = ({ nativeEvent }) => {
+        const { offsetX, offsetY } = nativeEvent;
+        contextRef.current.beginPath();
+        contextRef.current.moveTo(offsetX, offsetY);
+        setIsDrawing(true);
+    };
+
+    const draw = ({ nativeEvent }) => {
+        if (!isDrawing) return;
+        const { offsetX, offsetY } = nativeEvent;
+        contextRef.current.lineTo(offsetX, offsetY);
+        contextRef.current.stroke();
+      };
+
+      const finishDrawing = () => {
+        setIsDrawing(false);
+        contextRef.current.closePath();
+      };
+
+      const clearCanvas = () => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
+      };
 
     return (
         <div className='canvasDiv'>
-            <Canvas draw={draw} />
-            <p>Work in progress...</p>
+            <canvas id='canvas'
+            onMouseDown = {startDrawing}
+            onMouseUp = {finishDrawing}
+            onMouseMove = {draw}
+            ref = {canvasRef}
+            />
+            <button onClick={clearCanvas}>Clear</button>
         </div>
     )
 }
