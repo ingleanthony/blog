b3ee75543b2f822c8f86a3ee335a749a97e729a9
Anthony Ingle <ingleanthony@icloud.com>
2021-04-09 12:34:17 -0400

    Mobile responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/navbar.css';
 
 export default function Navbar() {
+
+  const [show, setShow] = useState(0);
+
+  function dropdown() {
+    (show) ? setShow(0) : setShow(1);
+    console.log(show);
+  }
+
   return (
     <nav>
       <h2 className="nav-title">Anthony's Blog</h2>
+      <div className='arrow-div' onClick={dropdown}><p className="arrow" show={show}></p></div>
       <ul>
-        <li>
+        <li show={show}>
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li show={show}>
           <Link to="/rationale">Rationale</Link>
         </li>
-        <li>
+        <li show={show}>
           <Link to="/about">About</Link>
         </li>
       </ul>
--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ export default function About() {
             <img className='about-pic' src={aboutPic} alt='me'/>
             <h1>Hi my name is Anthony.</h1>
             <p className='post-body'>
-                I am a student at Florida State University studying Computer Science. I am also working to be a full-stack web developer and create websites that impact people in a positive way. I am writing this blog in order for people to have a better and brighter understanding of artificial intelligence.
+                I am a student at Florida State University studying Computer Science. I am also working to be a full-stack web developer and create websites that impact people in a positive way. I am writing this blog in order for people to have a better and brighter understanding about artificial intelligence.
             </p>
         </div>
     )
